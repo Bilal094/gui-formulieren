c45857c51604197f37164f3fff642a71f77327d2
Bilal094 <99066318@mydavinci.nl>
2022-05-10 10:07:54 +0200

    dambord gemaakt

---
+++ dambord.py
@@ -0,0 +1,25 @@
+import tkinter as tk
+from tkinter import *
+
+root = tk.Tk()
+
+color = ['black', 'white']
+colorIndex = 0
+Row = 0
+root.resizable(False, False)
+
+def generator(count,rowCount):
+    global colorIndex
+    label = Label(bg=color[colorIndex])
+    label.grid(column=count,row=rowCount, ipadx=50,ipady=65)
+    if colorIndex == 0:
+        colorIndex = 1
+    else:
+        colorIndex = 0
+    
+for x in range(1,6):
+    for y in range(5):
+        generator(y,Row) 
+    Row += 1
+
+root.mainloop()
