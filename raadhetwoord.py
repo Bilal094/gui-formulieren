c193ccb2518dae7929ada9cdc345fd7469d7db7a
Bilal Achrifi <bilalachrifi@hotmail.com>
2022-05-19 22:33:48 +0200

    Edited the GUI settings

--- raadhetwoord.py
+++ raadhetwoord.py
@@ -10,24 +10,26 @@ wordEntry = StringVar()
 
 def player2():
     word = wordEntry.get()
-    if len(word) < 4:
+    if len(word) > 0 and len(word) < 4:
         showinfo('Fout', 'Jouw woord heeft minder dan 4 letters!')
     elif len(word) > 7:
         showinfo('Fout', 'Jouw woord heeft meer dan 7 letters!')
+    elif len(word) == 0:
+        showinfo('Fout', 'Vul eerst een woord in')
     else:
         root1.destroy()
         root2 = tk.Tk()
         root2.geometry('550x350')
         root2.title('Raad het woord - Speler 2')
         root2.resizable(False, False)
-
+        
 
 
         root2.mainloop()
 
 
 
-label1 = Label(text='Vul het woord in', font=('Helvetica', 15)).place(x=155,y=75)
+label1 = Label(text='Type het woord in', font=('Helvetica', 15)).place(x=150,y=75)
 label2 = Label(text='Je woord moet 4 tot 7 letters bevatten').place(x=130,y=150)
 entry1 = Entry(textvariable=wordEntry).place(x=165,y=120)
 button1 = Button(text='Klaar', relief='ridge', font=(14), command=player2).place(x=200,y=200)
