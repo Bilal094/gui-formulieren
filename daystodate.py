26ee2a469cb5d05f1f6b3751f4734299784614fc
Bilal094 <99066318@mydavinci.nl>
2022-05-19 10:09:12 +0200

    finished

--- daystodate.py
+++ daystodate.py
@@ -1,35 +1,47 @@
-from time import strftime
 import tkinter as tk
-import datetime
+from time import strftime
+from datetime import *
 from tkinter import *
 from tkinter import ttk
 from tkinter.messagebox import *
 
 # Main
+dateList = []
+newDateList = []
 
 def dayToMonth(event):
     global month
     month = monthSelect.get()
     if month in month31:
-        daySelect['values'] = ([i for i in range(32)])
+        daySelect['values'] = ([i for i in range(1,32)])
     if month in month30:
-        daySelect['values'] = ([i for i in range(31)])
+        daySelect['values'] = ([i for i in range(1,31)])
     if month in month28:
-        daySelect['values'] = ([i for i in range(29)])
+        daySelect['values'] = ([i for i in range(1,29)])
 
 def calculate():
-    day = int(daySelect.get())
     year = int(yearSelect.get())
+    month = datetime.strptime(monthSelect.get(),"%b")
+    day = int(daySelect.get())
     if year == int(dateToday.strftime("%Y")) and month == dateToday.strftime("%b") and day == int(dateToday.strftime("%d")):
         showinfo('Days to date calculator', 'Dit is vandaag')
     else:
-        pass
+        newDate = datetime(year,month.month,day)
+        dateExpression = abs(newDate-dateToday)
+        dateList.append(str(dateExpression))
+        for x in dateList:
+            x = x.split()
+        if newDate > dateToday:
+            showinfo('Days by date calculator', f'Dit is {x[0]} dagen in de toekomst')
+        elif newDate < dateToday:
+            showinfo('Days by date calculator', f'Dit is {x[0]} dagen in het verleden')
+        x.clear()
 
 # Root configuration
 month31 = ['Jan', 'Mar', 'May', 'Jul', 'Aug', 'Oct', 'Dec']
-month30 = ['Apr', 'Jun', 'Sept', 'Nov']
+month30 = ['Apr', 'Jun', 'Sep', 'Nov']
 month28 = ['Feb']
-dateToday = datetime.datetime.now()
+dateToday = datetime.now()
 
 root = tk.Tk()
 root.title('Days to date calculator')
@@ -48,7 +60,7 @@ label1 = Label(text='-',font=('Helvetica', 18)).place(x=225,y=193)
 
 monthSelect = ttk.Combobox(root,textvariable=month)
 monthSelect['state'] = 'readonly'
-monthSelect['values'] = ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec')
+monthSelect['values'] = ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec')
 monthSelect.place(x=260,y=200)
 label2 = Label(text='-', font=('Helvetica', 18)).place(x=425,y=193)
 monthSelect.bind('<<ComboboxSelected>>', dayToMonth)
